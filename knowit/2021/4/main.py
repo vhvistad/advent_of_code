fffb2e3532140c7fd5bf76eff22e83e76e98b88a
5Pax <vhvistad@gmail.com>
2021-12-05 00:39:54 +0100

    Knowit day 4

---
+++ knowit/2021/4/main.py
@@ -0,0 +1,34 @@
+import time
+import matplotlib.pyplot as plt
+
+start_time = time.time()
+
+def position_near_target(step):
+    diff = 8 + ((step-42) // 45) * 15
+    start_step = 42 + 45*((step-42) // 45)
+    x = (start_step + diff)//2
+    y = (start_step - diff)//2
+    return x, y, start_step
+    
+steps = 100000000000000000079
+x, y, start_step = position_near_target(steps)
+direction = 'north'
+diff = []
+
+for step in range(start_step, steps):
+    if direction == 'north':
+        y += 1
+        # print(f'{step}: {direction} ({x}, {y}) ', end='')
+        if y % 3 == 0 and y % 5 != 0:
+            direction = 'east'
+    elif direction == 'east':
+        x += 1
+        # print(f'{step}: {direction} ({x}, {y}) ', end='')
+        if x % 5 == 0 and x % 3 != 0:
+            direction = 'north'
+    diff.append(x-y)
+
+print(f'Final position: ({x}, {y})')
+print("--- {:.6f} seconds ---".format(time.time() - start_time))
+# plt.plot(diff)
+# plt.show()
