923e2b360e8d29f0c12cdaa22f966dd8a94df545
vhvistad <vhvistad@gmail.com>
2022-12-02 17:14:16 +0100

    Day 2 solution

--- knowit/2022/2/main.py
+++ knowit/2022/2/main.py
@@ -1,15 +1,7 @@
 import re
 
-test = "alver"
-test2 = "franske høner"
-
-print(re.search('alv', test))
-print(re.search('alv', test2))
-
-presents = []
-with open('knowit/2022/2/test.txt', 'r') as f:
-    for present in f:
-        presents.append(present)
+with open('knowit/2022/2/gaver.txt', 'r', encoding='utf-8') as f:
+    presents = f.readlines()
 
 linecount = 0
 last_presents = 0
@@ -21,10 +13,9 @@ for present in presents:
         if re.search('alv', present) is not None:
             last_presents -= 1
     else:
-        linecount += lines_to_add
         lines_to_add += 1
+        linecount += lines_to_add
         if re.search('alv', present) is not None:
             lines_to_add -=1
-    print(f'Lines to add: {lines_to_add}')
 
 print(linecount)
