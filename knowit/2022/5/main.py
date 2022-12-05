ede59a8bd68e92d1f69e96c05737ba20ef16bbe8
vegardvistad <vegard.vistad@ad.eyeshare.no>
2022-12-05 13:38:21 +0100

    Day 5 progress

---
+++ knowit/2022/5/main.py
@@ -0,0 +1,43 @@
+def shuffle(deck):
+    first, second = deck[:len(deck)//2], deck[len(deck)//2 + len(deck)%2:]
+    shuffled = []
+    for i in range(len(deck)):
+        if i % 2 == 0:
+            shuffled.append(first.pop(0))
+        else:
+            shuffled.append(second.pop(0))
+    return shuffled
+
+def shuffles_needed(original_deck):
+    times_shuffled = 0
+    shuffled_deck = []
+
+    while shuffled_deck != original_deck:
+        if times_shuffled == 0:
+            shuffled_deck = original_deck
+
+        shuffled_deck = shuffle(shuffled_deck)
+        times_shuffled += 1
+
+    return times_shuffled
+
+def make_deck(size):
+    deck = []
+
+    for i in range(1, size+1):
+        deck.append(i)
+    
+    return deck
+
+if __name__ == "__main__":
+    #deck = make_deck(52)
+    
+    size = 0
+    shuffles = 0
+    while shuffles != 13:
+        size += 2
+        deck = make_deck(size)
+        shuffles = shuffles_needed(deck)
+        print(shuffles)
+
+    print(size)
