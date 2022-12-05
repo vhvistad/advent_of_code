7131c001a6db85dc95840ca1522cfb76f7781005
vhvistad <vhvistad@gmail.com>
2022-12-05 20:25:52 +0100

    Day 5 solution

--- knowit/2022/5/main.py
+++ knowit/2022/5/main.py
@@ -10,38 +10,27 @@ def shuffle(deck):
             shuffled.append(second.pop(0))
     return shuffled
 
-def shuffles_needed(original_deck):
-    times_shuffled = 0
+def shuffle_13(original_deck):
     shuffled_deck = []
 
-    while shuffled_deck != original_deck:
-        if times_shuffled == 0:
+    for i in range(13):
+        if i == 0:
             shuffled_deck = original_deck
 
         shuffled_deck = shuffle(shuffled_deck)
-        times_shuffled += 1
 
-    return times_shuffled
+    return shuffled_deck == original_deck
 
-def make_deck(size):
-    deck = []
-
-    for i in range(1, size+1):
-        deck.append(i)
-    
-    return deck
 
 if __name__ == "__main__":
-    #deck = make_deck(52)
     start_time = time.time()
-
-    size = 0
-    shuffles = 0
-    while shuffles != 13:
+    deck = [1, 2]
+    size = len(deck)
+    while True:
+        deck = deck + [size + 1, size + 2]
         size += 2
-        deck = make_deck(size)
-        shuffles = shuffles_needed(deck)
-        print(f'Deck-size = {size}')
+        if shuffle_13(deck):
+            break
 
     print(f'Result: {size} cards.')
     print("--- Finished in %s seconds ---" % (time.time() - start_time))
